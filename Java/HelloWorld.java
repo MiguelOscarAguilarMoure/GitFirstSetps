d1a40e331712f8351f51cf125f6b6167984e44ef
Miguel Óscar Aguilar Moure <miguelomg421@gmail.com>
2025-11-10 11:46:49 +0100

    Update HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GitHUB!");
         System.out.println("Adding some changes!");
+        System.out.print("Moar changes lol");
     }
 }
